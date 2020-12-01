3fc47eb3a402e1742d08b2d9798ee493863af1fc
mathess8724 <mathesmsm@gmail.com>
2020-12-01 18:44:30 +0000

    static page

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,6 +56,10 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
 
 
 /***************************************************************************************************
