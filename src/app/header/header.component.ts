f48449aac1658a2867bbbcf37b9c62566eed791a
mathess8724 <mathesmsm@gmail.com>
2020-12-04 11:39:59 +0000

    prismic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,11 +6,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  Prismic = require('prismic-javascript');
+  PrismicData = 'test';
+
+  changeTest(){
+    this.PrismicData = 'changed!';
+  }
+
+  getPrismic(){
+
+  let   Prismic = require('prismic-javascript');
+   let  apiEndpoint = "https://sunrise-app.cdn.prismic.io/api/v2";
+    Prismic.default.getApi(apiEndpoint, ).then(function(api : any) {
+      return api.query(""); // An empty query will return all the documents
+    }).then(function(response : any) {
+      //console.log("Documents: ", response.results);
+      return response.results
+
+    }, function(err : any) {
+      console.log("Something went wrong: ", err);
+    });
+  }
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+
+    //this.PrismicData = this.getPrismic();
+    console.log(this.PrismicData)
+
   }
 
 }
+
+/* var Prismic = require('prismic-javascript');
+
+var apiEndpoint = "https://your-repository-name.cdn.prismic.io/api/v2";
+
+Prismic.getApi(apiEndpoint, { req: req }).then(function(api) {
+  return api.query(""); // An empty query will return all the documents
+}).then(function(response) {
+  console.log("Documents: ", response.results);
+}, function(err) {
+  console.log("Something went wrong: ", err);
+}); */
